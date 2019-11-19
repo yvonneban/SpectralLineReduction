44d4ad5c54774045373aa982b6bbeab66e84018e
Gopal Narayanan <gopastro@gmail.com>
2019-11-18 21:56:04 -0500

    Use glob rather than fnmatch - as it is a nicer wrapper and is platform agnostic

--- lmtslr/utils/ifproc_file_utils.py
+++ lmtslr/utils/ifproc_file_utils.py
@@ -1,15 +1,22 @@
 import os
-import fnmatch
+#import fnmatch
+import glob
 
-def lookup_ifproc_file(obsnum,path='/data_lmt/ifproc/'):
-    filename = ''
-    for file in os.listdir(path):
-        if fnmatch.fnmatch(file,'*_%06d_*.nc'%(obsnum)):
-            print('found %s'%(file))
-            filename = path+file
-    if filename == '':
-        print('lookup_ifproc_file: no file for obsnum ', obsnum)
-        if 'lmttpm' not in path:
-            print('look in lmttpm')
-            return lookup_ifproc_file(obsnum,path='/data_lmt/lmttpm/')
-    return(filename)
+def lookup_ifproc_file(obsnum, path='/data_lmt/ifproc/'):
+    # glob is a wrapper around fnmatch and takes care of looping through
+    # directories already
+    filenames = glob.glob(os.path.join(path, '*_%06d_*.nc' % obsnum))
+    if len(filenames) > 0:
+        print('found %s' % (filenames[0]))
+        return filenames[0]
+    #filename = ''
+    #for file in os.listdir(path):
+    #    if fnmatch.fnmatch(file,'*_%06d_*.nc'%(obsnum)):
+    #        print('found %s'%(file))
+    #        filename = path+file
+    #if filename == '':
+    print('lookup_ifproc_file: no file for obsnum ', obsnum)
+    if 'lmttpm' not in path:
+        print('look in lmttpm')
+        return lookup_ifproc_file(obsnum,path='/data_lmt/lmttpm/')
+    #return(filename)
--- lmtslr/utils/roach_file_utils.py
+++ lmtslr/utils/roach_file_utils.py
@@ -1,10 +1,11 @@
 import os
 import fnmatch
+import glob
 
 roach_pixels_all = [[0,1,2,3],[4,5,6,7],[8,9,10,11],[12,13,14,15]]
 
 def lookup_roach_files(obsnum,
-                       roach_list=['roach0/', 'roach1/', 'roach2/', 'roach3/'],
+                       roach_list=['roach0', 'roach1', 'roach2', 'roach3'],
                        path='/data_lmt/spectrometer/'):
     """ stand alone function to look up roach files which match a particular obsnum
         obsnum     is number to be matched
@@ -15,33 +16,32 @@ def lookup_roach_files(obsnum,
     nroach = len(roach_list)
     filenames = []
     result = 0
-    for i in range(nroach):
-        for file in os.listdir(path+roach_list[i]):
-            prefix = roach_list[i][:-1]
-            if fnmatch.fnmatch(file,'%s_%d_*.nc'%(prefix, obsnum)):
-                print('found %s'%(file))
-                if  not 'allantest' in file:
-                    print('append %s'%(file))
-                    filenames.append(path+roach_list[i]+file)
-                    result = result + 1
+    for roach in roach_list:
+        spec_filenames = glob.glob(os.path.join(path, roach, '%s_%d_*.nc' % (roach, obsnum)))
+        for filename in spec_filenames:
+            print('found %s' % (filename))
+            if  not 'allantest' in filename:
+                print('append %s' % (filename))
+                filenames.append(filename)
+                result = result + 1
     if filenames == []:
         print('lookup_roach_files: no files for obsnum', obsnum)
-    return(filenames,result)
+    return(filenames, result)
 
 
 def find_roach_from_pixel(pixel_id):
-    """ find roach from pixel 
+    """ 
+    find roach from pixel 
     """
-    for i,l in enumerate(roach_pixels_all):
-        if pixel_id in l:
+    for i, lis in enumerate(roach_pixels_all):
+        if pixel_id in lis:
             return [i]
-
     return []
 
 def create_roach_list(pixel_list):
     """ build a list of roach boards to be read given a pixel list
     """
-    rid = [0,0,0,0]
+    rid = [0, 0, 0, 0]
     for pixel_id in pixel_list:
         r = find_roach_from_pixel(pixel_id)
         if r != []:
@@ -49,5 +49,5 @@ def create_roach_list(pixel_list):
     roach_list = []
     for i in range(4):
         if rid[i] == 1:
-            roach_list.append('roach%d/'%(i))
+            roach_list.append('roach%d' % (i))
     return roach_list
